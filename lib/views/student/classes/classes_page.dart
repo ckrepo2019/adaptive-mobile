0c26770d39da646e1a94e8eea316e2623a56778a
Jdimarucut <dimarucut.paolo0@gmail.com>
2025-08-14 14:36:16 +0800

    ongoing classes page

---
+++ lib/views/student/classes/classes_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClassesPage extends StatelessWidget {
+  const ClassesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text("Something")
+        ),
+    );
+  }
+}
