491a4cdb1ec9195b1c16f381519d1fd695d68be0
yhnalx <jalix2003@gmail.com>
2025-08-14 14:51:22 +0800

    ongoing classes page

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   // Change this to your Laravel server URL
   static const String baseURL = 'http://192.168.50.40:8000/api/flutter';
+  
 }
---
+++ lib/views/student/classes/classes_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class StudentClassPage extends StatelessWidget {
+  const StudentClassPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Text("Something")
+        ),
+    );
+  }
+}w
