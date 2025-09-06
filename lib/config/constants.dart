b864704948af49aee3cc9346d2af022ded16c61f
AngeloGetuaban <getuaban143@gmail.com>
2025-09-06 12:03:11 +0800

    logout done

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,9 +4,9 @@ class AppConstants {
   // Change this to your Laravel server URL
   // static const String baseURL = 'http://10.0.2.2:8000/api/flutter';
 
-  //uncomment this if you want to use other base url
-  // static const String baseURL = 'http://192.168.50.145:8000/api/flutter';
-  // static const String imageBaseUrl = 'http://192.168.50.145:8000/storage/';
+  // //uncomment this if you want to use other base url
+  // static const String baseURL = 'http://192.168.50.31:8000/api/flutter';
+  // static const String imageBaseUrl = 'http://192.168.50.31:8000:8000/storage/';
 
   // Lix WFH
   static const String baseURL = 'https://adaptivehub.essentiel.ph/api/flutter';
